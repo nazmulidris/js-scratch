2a0898353240c2d12d4499182edb3f200bfaf7ad
Nazmul Idris <nazmul@fasterlap.com>
2019-04-01 17:45:58 -0700

    Minor refactor of programmatic HTML select element value change

--- html-element-select-scratch/script.js
+++ html-element-select-scratch/script.js
@@ -10,18 +10,20 @@ selectElement.addEventListener(
 /**
  * Programmatically change the option that's selected in the HTML select
  * element. Also fires a `change` event on the select element.
- * @param value Try and pick this option if it exists.
+ * @param selectElementSelector The selector for the HTML select element.
+ * @param optionValue Try and pick this option if it exists.
  */
-const setSelectedIndexAndFireChangeEvent = (value) => {
-    const selectElement =
-        document.querySelector('.ice-cream');
-    for (let i = 0; i < selectElement.options.length; i++) {
-        if (selectElement.options[i].text === value) {
-            selectElement.options[i].selected = true;
-            selectElement.dispatchEvent(new Event('change'));
-            return;
+const setSelectedIndexAndFireChangeEvent =
+    (selectElementSelector, optionValue) => {
+        const selectElement =
+            document.querySelector(selectElementSelector);
+        for (let i = 0; i < selectElement.options.length; i++) {
+            if (selectElement.options[i].text === optionValue) {
+                selectElement.options[i].selected = true;
+                selectElement.dispatchEvent(new Event('change'));
+                return;
+            }
         }
-    }
-};
+    };
 
 setSelectedIndexAndFireChangeEvent('sardine');
