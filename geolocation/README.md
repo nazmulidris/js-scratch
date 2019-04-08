af19b2f1836500c3d7f2be7ccc09113d7f0a06b4
Nazmul Idris <nazmul@google.com>
2019-04-08 15:48:09 -0700

    Add sample for geolocation

---
+++ geolocation/README.md
@@ -0,0 +1,5 @@
+# References
+
+- https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
+- https://developer.mozilla.org/en-US/docs/Web/API/Window/navigator
+- https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
---
+++ geolocation/script.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019 Nazmul Idris All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const nav = window.navigator;
+const geo = window.navigator.geolocation;
+
+class PositionListener {
+  /**
+   * @param {Position} position
+   */
+  onSuccess(position) {
+    const {latitude: lat, longitude: lng} = position.coords;
+    console.log(lat, lng);
+  }
+
+  /**
+   * @param {PositionError} error
+   */
+  onFailure(error) {
+    console.error(error.message);
+  }
+
+  options = {
+    maximumAge: 0,
+    timeout: 5000,
+    enableHighAccuracy: true,
+  };
+}
+
+const main = () => {
+  if (geo) {
+    console.log('geolocation api available in browser');
+    const positionListener = new PositionListener();
+    geo.getCurrentPosition(positionListener.onSuccess,
+        positionListener.onFailure, positionListener.options);
+  }
+  console.log('userAgent: ', nav.userAgent);
+};
+
+main();
