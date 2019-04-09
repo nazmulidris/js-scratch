8c34fa5dd134e6cdd6cce5fff0ef448277755e6c
Nazmul Idris <nazmul@google.com>
2019-04-09 08:29:20 -0700

    Update geolocation sample

--- geolocation/script.js
+++ geolocation/script.js
@@ -14,8 +14,76 @@
  * limitations under the License.
  */
 
-const nav = window.navigator;
-const geo = window.navigator.geolocation;
+let watchId = undefined;
+
+const main = () => {
+
+  console.log('userAgent: ', window.navigator.userAgent);
+
+  document.getElementById('getCurrentPosition').addEventListener("click",
+      (event) => {
+        assertGeoIsAvailable();
+        const listener = new PositionListener();
+        window.navigator.geolocation.getCurrentPosition(listener.onSuccess,
+            listener.onFailure,
+            listener.lowAccuracyOptions);
+      });
+
+  document.getElementById('watchPosition').addEventListener("click",
+      (event) => {
+        assertGeoIsAvailable();
+        const listener = new PositionListener();
+        if (watchId) {
+          console.log(`watchId: ${watchId} is already active.`);
+          return;
+        }
+        watchId = window.navigator.geolocation.watchPosition(listener.onSuccess,
+            listener.onFailure,
+            listener.lowAccuracyOptions);
+      });
+
+  document.getElementById('clearWatch').addEventListener("click",
+      (event) => {
+        assertGeoIsAvailable();
+        if (watchId) {
+          window.navigator.geolocation.clearWatch(watchId);
+        }
+      });
+};
+
+// Attach to window lifecycle events.
+
+/**
+ * More info: https://stackoverflow.com/a/52358522/2085356
+ */
+window.addEventListener('beforeunload', (event) => {
+  if (watchId) {
+    assertGeoIsAvailable().clearWatch(watchId);
+    console.log('clear watchId')
+  }
+  // Cancel the event as stated by the standard.
+  event.preventDefault();
+  // Chrome requires returnValue to be set.
+  event.returnValue = '';
+});
+
+window.addEventListener('DOMContentLoaded',
+    (event) => {
+      main();
+    });
+
+// Helper functions.
+
+const assertGeoIsAvailable = () => {
+  if (window.navigator.geolocation) {
+    return;
+  }
+  const message = "window.navigator.geolocation is not available";
+  console.error(message);
+  throw new Error(message);
+};
+
+// Helper classes.
 
 class PositionListener {
   /**
@@ -24,6 +92,9 @@ class PositionListener {
   onSuccess(position) {
     const {latitude: lat, longitude: lng} = position.coords;
     console.log(lat, lng);
+    document.getElementById(
+        'latLng').innerText = `${watchId ? "no watchId"
+        : watchId}, ${new Date()}: ${lat}, ${lng}`;
   }
 
   /**
@@ -33,37 +104,13 @@ class PositionListener {
     console.error(error.message);
   }
 
-  options = {
+  lowAccuracyOptions = {
+    timeout: 5000,
+    enableHighAccuracy: false,
+  };
+  highAccuracyOptions = {
     maximumAge: 0,
     timeout: 5000,
     enableHighAccuracy: true,
   };
 }
-
-let watchId = undefined;
-
-const main = () => {
-  console.log('userAgent: ', nav.userAgent);
-  if (geo) {
-    console.log('geolocation api available in browser');
-    const positionListener = new PositionListener();
-    geo.getCurrentPosition(positionListener.onSuccess,
-        positionListener.onFailure, positionListener.options);
-    watchId = geo.watchPosition(positionListener.onSuccess,
-        positionListener.onFailure);
-  }
-};
-
-window.addEventListener('beforeunload', function (e) {
-  if(geo && watchId){
-    geo.clearWatch(watchId);
-    console.log('clear watchId')
-  }
-
-  // Cancel the event as stated by the standard.
-  e.preventDefault();
-  // Chrome requires returnValue to be set.
-  e.returnValue = '';
-});
-
-main();
