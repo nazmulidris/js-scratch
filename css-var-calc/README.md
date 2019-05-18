c0807ea1644259b9091ada49a1cd83ed67c36607
Nazmul Idris <nazmul@fasterlap.com>
2019-05-18 11:02:56 -0700

    CSS variables and event delegation example

---
+++ css-var-calc/README.md
@@ -0,0 +1,14 @@
+# References
+
+CSS calc and variables
+- https://developer.mozilla.org/en-US/docs/Web/CSS/calc
+- https://medium.freecodecamp.org/everything-you-need-to-know-about-css-variables-c74d922ea855
+- https://css-tricks.com/a-couple-of-use-cases-for-calc/
+- https://stackoverflow.com/questions/37801882/how-to-change-css-root-color-variables-in-javascript
+- https://css-tricks.com/updating-a-css-variable-with-javascript/
+
+CSS box sizing
+- https://developer.mozilla.org/en-US/docs/Web/CSS/box-sizing
+
+HTML input event
+- https://stackoverflow.com/questions/7060750/detect-the-enter-key-in-a-text-input-field
---
+++ css-var-calc/script.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 Nazmul Idris All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const main = () => {
+  document.querySelector('#root')
+          .addEventListener(
+              'keyup', (event) => {
+                const inputMatches = event.target.matches('input');
+                if (!inputMatches) return;
+                if (event.key === "Enter") {
+                  const inputElement = event.target;
+                  const rootElement = document.documentElement;
+                  switch (inputElement.name) {
+                    case "font-family":
+                      rootElement.style.setProperty(
+                          '--page-font',
+                          inputElement.value
+                      );
+                      break;
+                    case "font-size":
+                      rootElement.style.setProperty(
+                          '--page-font-size',
+                          inputElement.value
+                      );
+                      break;
+                  }
+                }
+              })
+};
+
+main();
